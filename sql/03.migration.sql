af619edd1607146eb6d1b209421a46d05e6f2611
rozumek <marcin.wyrozumski@gmail.com>
2013-09-22 19:41:17 +0200

    build fixes

--- sql/03.migration.sql
+++ sql/03.migration.sql
@@ -2,7 +2,7 @@
 insert into clients(name, email, broker) select distinct customer, 'no-email@email.com', broker from stock_orders
 
 -- changing customer name to id
-update stock_orders set customer = (select id from clients c where c.name = customer)
+update stock_orders s set customer = (select id from clients c where c.name = customer and c.broker = s.broker)
 
 -- changing customer column type from varchar to int
 alter table stock_orders modify customer int
