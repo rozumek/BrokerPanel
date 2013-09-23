5a372cb8c305a18a76755ff165b2d8cd721cbbfa
rozumek <marcin.wyrozumski@gmail.com>
2013-09-23 20:37:40 +0200

    Stock order changes

--- public/js/stock-orders.js
+++ public/js/stock-orders.js
@@ -3,14 +3,26 @@
         /**
          * Stock Order form submition
          */
-        $('#stockOrderForm').submit(function(){            
+        $('#stockOrderForm').submit(function(){
             var submitForm = true;
 
             if($('#stopLossValue').val() == '' && $('#stockOrderForm input[name="type"]').val() != 2){
-                submitForm = confirm($('#stopLossValue').attr('warning'))
+                submitForm = confirm($('#stopLossValue').attr('warning'));
             }
-            
+
             return submitForm
-        });        
+        });
+
+        /**
+         * Register Order form limit value processing
+         */
+        $('input[name="limit_value_type"]').change(function(){
+            if ($(this).val() == 2) {
+                $('#limit_value').attr('disabled', true);
+            } else {
+                $('#limit_value').attr('disabled', false);
+            }
+        });
+        $('input[name="limit_value_type"]:checked').trigger('change');
     });
 })(jQuery);
